894cdb559bcb5abbac57090e99cb7071ea127bd4
vaquitamarina <cristhianvm55@gmail.com>
2024-02-28 21:29:35 -0500

    first commit

---
+++ polybar/launch.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Terminate already running bar instances
+killall -q polybar
+
+# If all your bars have ipc enabled, you can also use
+# polybar-msg cmd quit
+
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+# Launch Polybar, using default config location ~/.config/polybar/config.ini
+polybar &
+
+
+echo "Polybar launched..."
+
+
+
